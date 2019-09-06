0b9ac5ec83e5326cc2f2aabf72d20c385bf05ad8
jkaunert <44586402+jkaunert@users.noreply.github.com>
2019-09-06 14:01:23 -0500

    Added starter playground + updated README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# make-change-from-string-swift-challenge
+### Challenge
+
+Create a function `makeChangeAsString(fromAmount:withCost:)` that takes two `Doubles` as parameters - `fromAmount`(the amount given to pay for the purchase) and `withCost`(how much the item costs) and returns a `String` that details the total amount of change returned _and_ the quantity and type of each denomination returned (in dollars, quarters, dimes, nickels and pennies).
+
+#####Example:
+
+`makeChangeAsString(fromAmount: 5.00, withCost: 2.15)  // returns "Your change is $2.85. That is 2 dollars, 3 quarters, 1 dime, 0 nickels and 0 pennies."`
+
+`makeChangeAsString(fromAmount: 10.00, withCost: 2.38) // returns "Your change is $7.62. That is 7 dollars, 2 quarters, 1 dime, 0 nickels and 2 pennies."
+`
+
+Here are the overall [instructions for code challenges](https://github.com/LambdaSchool/ios-code-challenge-instructions). Before you begin, fork and clone this repo and work through your solution in the included starter playground file. When you're done, **Please make sure to save and push all your work, and submit a Pull Request. Don't forget tag your TL so they can review your submission!**
---
+++ makeChangeAsString.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Test Cases
+//makeChangeAsString(fromAmount: 10.00, withCost: 2.38) // returns "Your change is $7.62. That is 7 dollars, 2 quarters, 1 dime, 0 nickels and 2 pennies."
+//makeChangeAsString(fromAmount: 5.00, withCost: 2.15)  // returns "Your change is $2.85. That is 2 dollars, 3 quarters, 1 dime, 0 nickels and 0 pennies."
+//makeChangeAsString(fromAmount: 1.00, withCost: 2.38)  // returns "You didn't pay enough! You still owe $1.38. That is 1 dollar, 1 quarter, 1 dime, 0 nickels and 3 pennies."
+//makeChangeAsString(fromAmount: 10.00, withCost: 8.00) // returns "Your change is $2.0. That is 2 dollars, 0 quarters, 0 dimes, 0 nickels and 0 pennies."
+//makeChangeAsString(fromAmount: 1.00, withCost: 0.50)  // returns "Your change is $0.5. That is 0 dollars, 2 quarters, 0 dimes, 0 nickels and 0 pennies."
+//makeChangeAsString(fromAmount: 1.00, withCost: 0.80)  // returns "Your change is $0.2. That is 0 dollars, 0 quarters, 2 dimes, 0 nickels and 0 pennies."
+//makeChangeAsString(fromAmount: 1.00, withCost: 0.95)  // returns "Your change is $0.05. That is 0 dollars, 0 quarters, 0 dimes, 1 nickel and 0 pennies."
+//makeChangeAsString(fromAmount: 1.00, withCost: 0.96)  // returns "Your change is $0.04. That is 0 dollars, 0 quarters, 0 dimes, 0 nickels and 4 pennies."
